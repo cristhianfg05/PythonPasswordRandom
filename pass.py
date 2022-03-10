455d4c395a2a3c9d81ae9d9cf1867dacac87cb4b
cristhian fajardo <cristhianfg.cfg@gmail.com>
2022-03-10 10:53:46 +0100

    Update pass.py

--- pass.py
+++ pass.py
@@ -1,13 +1,20 @@
 import random
 
-minusculas = "abcdefghijklmnopqrstuvwxyzñ"
-mayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZÑ"
-numeros = "1234567890"
-simbolos = "(){}[]*·:;/-_"
+#Dim (password length)
+#sim (boolean simbols)
+def passRandom(dim, sim):
+    minusculas = "abcdefghijklmnopqrstuvwxyzñ"
+    mayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZÑ"
+    numeros = "1234567890"
+    simbolos = "(){}[]*·:;/-_"
 
-todos = minusculas + mayusculas + numeros + simbolos
+    if sim: #password with simbols
+        todos = minusculas + mayusculas + numeros + simbolos
+    else:   #password withouth simbols
+        todos = minusculas + mayusculas + numeros
+    
+    clave = "".join(random.sample(todos,dim))
+    return clave
 
-dim = 16
+print (passRandom(5, False))
 
-clave = "".join(random.sample(todos,dim))
-print (clave)
